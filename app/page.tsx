7c11ac9f33b05787ba1ad8038ba0e3644a59cc03
Angelo Aad <angelo.aad1@gmail.com>
2025-01-22 15:25:05 +0200

    Assets And MetaData

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <h1>Welcome to the world of Next.js</h1>
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to the world of Next.js (Inter)</h1>
+    </>
   );
 }
