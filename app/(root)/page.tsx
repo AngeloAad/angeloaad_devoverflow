974137886767a86c17fc101378c90625bf5bc6c0
Angelo Aad <angelo.aad1@gmail.com>
2025-02-01 14:29:33 +0200

    Implement Local Search Bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,39 @@
-import { auth, signOut } from "@/auth";
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import LocalSearch from "@/components/search/LocalSearch";
 
-export default async function Home() {
-  const session = await auth();
-
-  console.log(session);
-  
+const Home = async () => {
   return (
     <>
-      <h1>Home</h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
-      <form className="px-10 pt-[100 px]" action={async () => {
-        "use server";
-
-        await signOut({ redirectTo: ROUTES.SIGN_IN });
-      }}>
-       
-      </form>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11"> 
+        <LocalSearch 
+        route="/"
+        imgSrc="/icons/search.svg"
+        placeholder="Search for Questions Here..."
+        otherClasses="flex-1"
+        /> 
+      </section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
     </>
   );
-}
+};
+
+export default Home;
