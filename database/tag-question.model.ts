a7a54068f025e153db783d82c6660cab6022142f
Angelo Aad <angelo.aad1@gmail.com>
2025-02-08 10:24:58 +0200

    Create Vote Model

--- database/tag-question.model.ts
+++ database/tag-question.model.ts
@@ -3,17 +3,21 @@ import { model, models, Schema, Types } from "mongoose";
 
 // 1. Create an interface representing a document in MongoDB.
 export interface ITagQuestion {
-    question: Types.ObjectId;
-    tagId: Types.ObjectId;
+  question: Types.ObjectId;
+  tagId: Types.ObjectId;
 }
 
 // 2. Create a Schema corresponding to the document interface.
-const TagQuestionSchema = new Schema<ITagQuestion>({
+const TagQuestionSchema = new Schema<ITagQuestion>(
+  {
     question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
     tagId: { type: Schema.Types.ObjectId, ref: "Tag", required: true },
-}, {timestamps: true});
+  },
+  { timestamps: true }
+);
 
 // 3. Create a TagQuestion.
-const TagQuestion = models?.TagQuestion || model<ITagQuestion>("TagQuestion", TagQuestionSchema);
+const TagQuestion =
+  models?.TagQuestion || model<ITagQuestion>("TagQuestion", TagQuestionSchema);
 
 export default TagQuestion;
---
+++ database/vote.model.ts
@@ -0,0 +1,23 @@
+// This is a template model for creating a new model
+import { model, models, Schema, Types } from "mongoose";
+
+// 1. Create an interface representing a document in MongoDB.
+export interface IVote {
+    author: Types.ObjectId;
+    id: Types.ObjectId;
+    type: "question" | "answer";
+    voteType:  "upvote" | "downvote";
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const VoteSchema = new Schema<IVote>({
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    id: { type: Schema.Types.ObjectId, required: true },
+    type: { type: String, enum: ["question", "answer"], required: true },
+    voteType: { type: String, enum: ["upvote", "downvote"], required: true },
+}, {timestamps: true});
+
+// 3. Create a Vote.
+const Vote = models?.Vote || model<IVote>("Vote", VoteSchema);
+
+export default Vote;
