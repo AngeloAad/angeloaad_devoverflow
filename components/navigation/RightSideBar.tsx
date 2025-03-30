73c683c4fd9d6fdf3cc30d78476922bafbaeb160
Angelo Aad <angelo.aad1@gmail.com>
2025-03-30 07:38:24 +0300

    Improved Hot Questions and Popular Tags by Implementing Parallel Data Fetching

--- components/navigation/RightSideBar.tsx
+++ components/navigation/RightSideBar.tsx
@@ -8,10 +8,13 @@ import { getPopularTags } from "@/lib/actions/tag.action";
 import DataRenderer from "../DataRenderer";
 
 const RightSidebar = async () => {
-  const { success: successHotQuestions, data: dataHotQuestions, error: errorHotQuestions } = await getHotQuestions();
-  const hotQuestions = dataHotQuestions || [];
-  const { success: successPopularTags, data: dataPopularTags, error: errorPopularTags } = await getPopularTags();
-  const popularTags = dataPopularTags || [];
+  const [
+    { success: successHotQuestions, data: dataHotQuestions, error: errorHotQuestions },
+    { success: successPopularTags, data: dataPopularTags, error: errorPopularTags },
+  ] = await Promise.all([
+    getHotQuestions(),
+    getPopularTags(),
+  ]);
 
   return (
     <section className="pt-36 custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 shadow-light-300 dark:shadow-none max-xl:hidden">
@@ -19,7 +22,7 @@ const RightSidebar = async () => {
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
 
         <DataRenderer
-          data={hotQuestions}
+          data={dataHotQuestions}
           success={successHotQuestions}
           error={errorHotQuestions}
           empty={{
@@ -53,18 +56,18 @@ const RightSidebar = async () => {
       <div className="mt-16">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
 
-        <div className="mt-7 flex flex-col gap-4">
+        <div className="flex flex-col gap-4">
           <DataRenderer
-            data={popularTags}
+            data={dataPopularTags}
             success={successPopularTags}
             error={errorPopularTags}
             empty={{
               title: "No tags found",
               message: "No tags have been created yet",
             }}
-            render={(popularTags) => (
+            render={(dataPopularTags) => (
               <div className="mt-7 flex flex-col gap-4">
-                {popularTags.map(({ _id, name, questions }) => (
+                {dataPopularTags.map(({ _id, name, questions }) => (
                   <TagCard
                     key={_id}
                     _id={_id}
