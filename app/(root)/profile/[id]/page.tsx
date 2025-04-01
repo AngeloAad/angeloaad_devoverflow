368a944931b9fe941dde7346faa58c9f6428ed13
Angelo Aad <angelo.aad1@gmail.com>
2025-04-01 06:32:13 +0300

    Implement Profile Tabs Layout UI

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import ProfileLink from "@/components/user/ProfileLink";
 import Stats from "@/components/user/Stats";
+import { TabsList, Tabs, TabsContent, TabsTrigger } from "@/components/ui/tabs";
 
 const Profile = async ({ params }: RouteParams) => {
   const { id } = await params;
@@ -120,6 +121,31 @@ const Profile = async ({ params }: RouteParams) => {
         }}
       />
 
+      <section className="mt-10 flex gap-10">
+        <Tabs defaultValue="top-posts" className="flex-[2]">
+          <TabsList className="background-light800_dark400 min-h-[42px] p-1">
+            <TabsTrigger value="top-posts" className="tab">
+              Top Posts
+            </TabsTrigger>
+            <TabsTrigger value="top-answers" className="tab">
+              Top Answers
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="top-posts" className="mt-5 flex w-full flex-col gap-6">
+            List of Questions here
+          </TabsContent>
+          <TabsContent value="top-answers" className="flex w-full flex-col gap-6">
+            List of Answers here
+          </TabsContent>
+        </Tabs>
+
+        <div className="flex w-full min-w-[250px] flex-1 flex-col max-lg:hidden">
+          <h3 className="h3-bold text-dark200_light900">Top Tech</h3>
+          <div className="mt-7 flex flex-col gap-4">
+            <p>List of Tags</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
